8cab63e06c28964298a75537683f99d18a8309d4
gazf <gazfff@gmail.com>
2016-11-08 10:39:34 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,17 @@ console.clear();
 
 var table = document.getElementById("grid");
 var paths = table.getElementsByClassName("leftmost path-cell");
-console.log(paths);
+
+function createButton(dom, option){
+  var button = document.createElement("button");
+  button.innerText = option.text;
+  button.addEventListener("click", option.listener);
+  dom.appendChild(button);
+}
+
+for(var i = 0; i < paths.length; i++){
+  var path = paths[i].innerText();
+  createButton(paths[i], {text: path, listener: function(e){
+    console.log(e);
+  }});
+}
