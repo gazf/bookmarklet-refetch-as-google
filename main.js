945013387a2289f6dfde4d1cc3a896c103706c2c
gazf <gazfff@gmail.com>
2016-11-08 10:14:56 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 console.clear();
 
 var table=document.getElementById("grid");
+console.log("row", table.rows.length);
 for(var i = 0; i < table.rows.length; i++){
+  console.log("cell", table.rows[i].length);
   for(var j = 0; j < table.rows[i].cells.length; j++){
-    console.log(table.rows[i].cells[j]);
+    console.log(i, j, table.rows[i].cells[j], table.rows[i].cells[j].innerText);
   }
 }
