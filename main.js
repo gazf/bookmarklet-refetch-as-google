624bb78c300eb154bb0a963d40615e5e6fb2965a
gazf <gazfff@gmail.com>
2016-11-08 10:23:00 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 console.clear();
 
-var table=document.getElementById("grid");
-console.log("row", table.rows.length);
-for(var i = 0; i < table.rows.length; i++){
-  console.log("cell", table.rows[i].length);
-  for(var j = 0; j < table.rows[i].cells.length; j++){
-    console.log(i, j, table.rows[i].cells[j], table.rows[i].cells[j].innerText);
-  }
-}
+var table = document.getElementById("grid");
+var paths = table.getElementsByClassName("leftmost path-cell");
+console.log(paths);
