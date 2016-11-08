ceddb18ea69f91fe17a56136fd77f693a2ee1397
gazf <gazfff@gmail.com>
2016-11-08 11:07:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,14 +8,18 @@ function createButton(dom, option){
   var button = document.createElement("button");
   button.innerText = option.text;
   button.addEventListener("click", option.listener);
+  button.setAttribute("data-path", option.path);
   dom.appendChild(button);
 }
 
 for(var i = 0; i < paths.length; i++){
   var path = paths[i].innerText;
-  createButton(paths[i], {text: path, listener: function(e){
+  createButton(paths[i], {text: path, path: path, listener: function(e){
     console.log(e);
     e.stopPropagation();
-    input.value = e.target.innerText;
+    
+    var path = e.target.getAttribute("data-path");
+    if(path.substr(0, 1) === "/") path = path.substr(1);
+    input.value = path;
   }});
 }
